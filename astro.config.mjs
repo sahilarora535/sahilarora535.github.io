61f27f50fde478b2d2d53145aa27ec5cd939f564
Sahil Arora <12633577+sahilarora535@users.noreply.github.com>
2025-02-07 08:34:06 +0530

    Add starlight-blog for blogging

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,13 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 import tailwind from '@astrojs/tailwind';
+import starlightBlog from 'starlight-blog';
 
 // https://astro.build/config
 export default defineConfig({
 	integrations: [
 		starlight({
+			plugins: [starlightBlog()],
 			title: 'Sahil Arora',
 			social: {
 				linkedin: 'https://www.linkedin.com/in/sahilarora535/',
