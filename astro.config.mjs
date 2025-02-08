b28f3f15bfb458fd33161c2c4b21e1d0f15bd737
Sahil Arora <12633577+sahilarora535@users.noreply.github.com>
2025-02-08 17:42:37 +0530

    Add D2 integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,41 +4,40 @@ import starlight from '@astrojs/starlight';
 import tailwind from '@astrojs/tailwind';
 import starlightBlog from 'starlight-blog';
 
+import d2 from 'astro-d2';
+
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://sahilarora.in',
-	integrations: [
-		starlight({
-			plugins: [starlightBlog()],
-			favicon: './src/assets/logo/logo-namaste-dark.png',
-			title: 'Sahil Arora',
-			description: 'Sahil Arora\'s personal blog and portfolio',
-			logo: {
-				src: './src/assets/logo/logo-meditate-dark.png',
-			},
-			lastUpdated: true,
-			social: {
-				linkedin: 'https://www.linkedin.com/in/sahilarora535/',
-				github: 'https://github.com/sahilarora535',
-				stackOverflow: 'https://stackoverflow.com/users/4554700/sahil-arora',
-				email: 'mailto:sight-cyclops.0a@icloud.com',
-			},
-			tableOfContents: true,
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-			customCss: ['./src/tailwind.css', '@fontsource/atkinson-hyperlegible/700.css', '@fontsource/atkinson-hyperlegible/400.css'],
-		}),
-		tailwind({ applyBaseStyles: false }),
-	],
+    site: 'https://sahilarora.in',
+    integrations: [starlight({
+        plugins: [starlightBlog()],
+        favicon: './src/assets/logo/logo-namaste-dark.png',
+        title: 'Sahil Arora',
+        description: 'Sahil Arora\'s personal blog and portfolio',
+        logo: {
+            src: './src/assets/logo/logo-meditate-dark.png',
+        },
+        lastUpdated: true,
+        social: {
+            linkedin: 'https://www.linkedin.com/in/sahilarora535/',
+            github: 'https://github.com/sahilarora535',
+            stackOverflow: 'https://stackoverflow.com/users/4554700/sahil-arora',
+            email: 'mailto:sight-cyclops.0a@icloud.com',
+        },
+        tableOfContents: true,
+        sidebar: [
+            {
+                label: 'Guides',
+                items: [
+                    // Each item here is one entry in the navigation menu.
+                    { label: 'Example Guide', slug: 'guides/example' },
+                ],
+            },
+            {
+                label: 'Reference',
+                autogenerate: { directory: 'reference' },
+            },
+        ],
+        customCss: ['./src/tailwind.css', '@fontsource/atkinson-hyperlegible/700.css', '@fontsource/atkinson-hyperlegible/400.css'],
+		}), tailwind({ applyBaseStyles: false }), d2()],
 });
