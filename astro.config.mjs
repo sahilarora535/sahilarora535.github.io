cbdd122a98e06d068c51f7a8a486d22317a83a63
Sahil Arora <12633577+sahilarora535@users.noreply.github.com>
2025-10-07 23:40:05 +0530

    chore: add tailwind css v4

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,40 +5,47 @@ import starlightBlog from 'starlight-blog';
 
 import d2 from 'astro-d2';
 
+import tailwindcss from '@tailwindcss/vite';
+
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://sahilarora.in',
-    integrations: [starlight({
-        plugins: [starlightBlog({
-            title: 'Blog',
-            postCount: 10,
-            recentPostCount: 10,
-        })],
-        head: [{
-            tag: 'script',
-            attrs: {
-                src: 'https://cloud.umami.is/script.js',
-                'data-website-id': "6905ce4e-a9b4-4fa2-874b-566b98d63957",
-                defer: true,
-            }
-        }],
-        favicon: './src/assets/logo/logo-namaste-dark.png',
-        title: 'Sahil Arora',
-        description: 'Sahil Arora\'s personal blog and portfolio',
-        logo: {
-            src: './src/assets/logo/logo-meditate-dark.png',
-        },
-        lastUpdated: true,
-        social: [
-            { icon: 'github', label: 'GitHub', href: 'https://github.com/sahilarora535' },
-            { icon: 'linkedin', label: 'LinkedIn', href: 'https://www.linkedin.com/in/sahilarora535/' },
-            { icon: 'stackOverflow', label: 'StackOverflow', href: 'https://stackoverflow.com/users/4554700/sahil-arora' },
-            { icon: 'email', label: 'EMail', href: 'mailto:sight-cyclops.0a@icloud.com' }
-        ],
-        tableOfContents: {
-            minHeadingLevel: 1,
-            maxHeadingLevel: 6,
-        },
-        customCss: ['@fontsource-variable/inter'],
-        }), d2()],
+  site: 'https://sahilarora.in',
+
+  integrations: [starlight({
+      plugins: [starlightBlog({
+          title: 'Blog',
+          postCount: 10,
+          recentPostCount: 10,
+      })],
+      head: [{
+          tag: 'script',
+          attrs: {
+              src: 'https://cloud.umami.is/script.js',
+              'data-website-id': "6905ce4e-a9b4-4fa2-874b-566b98d63957",
+              defer: true,
+          }
+      }],
+      favicon: './src/assets/logo/logo-namaste-dark.png',
+      title: 'Sahil Arora',
+      description: 'Sahil Arora\'s personal blog and portfolio',
+      logo: {
+          src: './src/assets/logo/logo-meditate-dark.png',
+      },
+      lastUpdated: true,
+      social: [
+          { icon: 'github', label: 'GitHub', href: 'https://github.com/sahilarora535' },
+          { icon: 'linkedin', label: 'LinkedIn', href: 'https://www.linkedin.com/in/sahilarora535/' },
+          { icon: 'stackOverflow', label: 'StackOverflow', href: 'https://stackoverflow.com/users/4554700/sahil-arora' },
+          { icon: 'email', label: 'EMail', href: 'mailto:sight-cyclops.0a@icloud.com' }
+      ],
+      tableOfContents: {
+          minHeadingLevel: 1,
+          maxHeadingLevel: 6,
+      },
+      customCss: ['./src/styles/global.css', '@fontsource-variable/inter', '@fontsource-variable/fira-code'],
+      }), d2()],
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
